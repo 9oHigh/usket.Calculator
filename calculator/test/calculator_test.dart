f95f97696e1baffd14488181e2f2cc97942338e1
9oHigh <wkdska957@naver.com>
2024-12-04 10:47:35 +0900

    test: add failure test for add operation

--- calculator/test/calculator_test.dart
+++ calculator/test/calculator_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:calculator/calculator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group("AddOperation", () {
+    test("return sum of two numbers", () {
+      final operation = AddOperation();
+      expect(operation.excute(1, 2), equals(3));
+    });
   });
 }
