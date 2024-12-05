d892a40a7d4f260abdb5b40a0f2e941c66050a99
9oHigh <wkdska957@naver.com>
2024-12-05 15:13:04 +0900

    refactor: operation factory 클래스에 삼각함수 place holder 코드 및 To do 추가

--- calculator/lib/input_parser.dart
+++ calculator/lib/input_parser.dart
@@ -1,9 +1,8 @@
 class InputParser {
   static List<double> parseOperands(String input, String operator) {
-    final numbers = input.split(' ').where((number) => number != operator).toList();
-    final operands = numbers
-        .map((part) => double.tryParse(part))
-        .toList();
+    final numbers =
+        input.split(' ').where((number) => number != operator).toList();
+    final operands = numbers.map((part) => double.tryParse(part)).toList();
 
     if (operands.any((operand) => operand == null)) {
       throw '입력이 옳바른지 확인하고 다시 입력해주세요.';
--- calculator/lib/operation/operation_factory.dart
+++ calculator/lib/operation/operation_factory.dart
@@ -5,7 +5,16 @@ import 'package:calculator/operation/operations/multiply_operation.dart';
 import 'package:calculator/operation/operations/subtract_operation.dart';
 
 class OperationFactory {
+  
   static Operation getOperation(String operator) {
+    if (['+', '-', '*', '/'].contains(operator)) {
+      return _getElementryOperation(operator);
+    } else {
+      throw '제공되지 않는 연산자: $operator';
+    }
+  }
+
+  static Operation _getElementryOperation(String operator) {
     switch (operator) {
       case '+':
         return AddOperation();
@@ -19,4 +28,18 @@ class OperationFactory {
         throw '제공되지 않는 연산자: $operator';
     }
   }
+  //TODO: implements SineOperation, CosineOperation and add a case to the getOperation method
+  /*
+  [Placeholder]
+  static Operation _getTrigonometricOperation(String operator) {
+    switch (operator) {
+      case 'sin':
+        return SineOperation();
+      case 'cos':
+        return CosineOperation();
+      default:
+        throw ArgumentError('Unsupported trigonometric operator: $operator');
+    }
+  } 
+  */
 }
